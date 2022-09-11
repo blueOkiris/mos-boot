0da73fa6621502925373f76b26b940efa1f82670
Dylan Turner <dylantdmt@gmail.com>
2022-09-11 18:51:25 -0500

    gdt.asm: added user segments and don't change data segment to 32-bit

--- stage2/gdt.asm
+++ stage2/gdt.asm
@@ -3,6 +3,10 @@
 
 [bits 16]
 
+; See https://wiki.osdev.org/GDT_Tutorial
+; and https://wiki.osdev.org/Global_Descriptor_Table#Segment_Descriptor
+; for reference
+
 gdt_null_desc:
     dd 0
     dd 0
@@ -23,6 +27,22 @@ gdt_data_desc:
     db 0b11001111       ; Large mem, 32 bit, 0, limit extension
     db 0x00             ; Base pt 2 (high)
 
+gdt_user_code_desc:
+    dw 0xFFFF           ; Limit (we set it to max)
+    dw 0x0000           ; Base (low) of code is just start of mem
+    db 0x00             ; The upper part of 24-bit address
+    db 0b11111010       ; Pres, Kernel Priv, Data or Code, Code, Readable
+    db 0b11001111       ; Large mem, 32 bit, 0, limit extension
+    db 0x00
+
+gdt_user_data_desc:
+    dw 0xFFFF           ; Limit (we set it to max)
+    dw 0x0000           ; Base (low) of code is just start of mem
+    db 0x00             ; The upper part of 24-bit address
+    db 0b11110010       ; Pres, Kernel Priv, Data or Code, Data, Readable
+    db 0b11001111       ; Large mem, 32 bit, 0, limit extension
+    db 0x00             ; Base pt 2 (high)
+
 gdt_end:
 
 gdt_desc:
@@ -37,6 +57,7 @@ data_seg    equ gdt_data_desc - gdt_null_desc
 [bits 32]
 
 edit_gdt:
+    ; Change the 0x0C flag to 0x0A for code segments to make them 64 bit
     mov     [gdt_code_desc + 6], byte 0b10101111
-    mov     [gdt_data_desc + 6], byte 0b10101111
+    mov     [gdt_user_code_desc + 6], byte 0b10101111
     ret
